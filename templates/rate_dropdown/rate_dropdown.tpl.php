24ab78b851b61c232fa76e42605d17aaa5594f4e
Paul Byrne <paul.drupal@leafish.co.uk>
2012-05-18 20:30:44 +0100

    Initial commit of .module and template file.

---
+++ templates/rate_dropdown/rate_dropdown.tpl.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Rate Drop-down widget theme
+ */
+
+if ($jump) {
+  print $jump;
+}
+
+if ($info) {
+  print '<div class="rate-info">' . $info . '</div>';
+}
+
+if ($display_options['description']) {
+  print '<div class="rate-description">' . $display_options['description'] . '</div>';
+}
+
+if ($admin_results) {
+  print $admin_results;
+}
